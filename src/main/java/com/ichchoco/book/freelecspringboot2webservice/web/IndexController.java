64b2e0d04c344cda2f7e842a0cffd9fb24fcc6c7
giyeon <qkrrldus93g@gmail.com>
2022-10-03 22:09:25 +0900

    feat : 4장 화면 구성

---
+++ src/main/java/com/ichchoco/book/freelecspringboot2webservice/web/IndexController.java
@@ -0,0 +1,19 @@
+package com.ichchoco.book.freelecspringboot2webservice.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+}
--- src/test/java/com/ichchoco/book/freelecspringboot2webservice/web/PostsApiControllerTest.java
+++ src/test/java/com/ichchoco/book/freelecspringboot2webservice/web/PostsApiControllerTest.java
@@ -4,6 +4,7 @@ package com.ichchoco.book.freelecspringboot2webservice.web;
 import com.ichchoco.book.freelecspringboot2webservice.domain.posts.Posts;
 import com.ichchoco.book.freelecspringboot2webservice.domain.posts.PostsRepository;
 import com.ichchoco.book.freelecspringboot2webservice.web.dto.PostsSaveRequestDto;
+import com.ichchoco.book.freelecspringboot2webservice.web.dto.PostsUpdateRequestDto;
 import org.assertj.core.api.Assertions;
 import org.junit.After;
 import org.junit.Test;
@@ -12,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -55,5 +58,40 @@ public class PostsApiControllerTest {
         List<Posts> all = postsRepository.findAll();
         Assertions.assertThat(all.get(0).getTitle()).isEqualTo(title);
         Assertions.assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void Posts_수정된다() throws Exception{
+        //given
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build()
+        );
+
+        Long updateId = savedPosts.getID();
+        String expectedTitle = "title2";
+        String expectedCotnent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedCotnent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        //then
+        Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        Assertions.assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        Assertions.assertThat(all.get(0).getContent()).isEqualTo(expectedCotnent);
     }
 }
